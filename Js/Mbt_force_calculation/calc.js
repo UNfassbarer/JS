571d2a618f82b84dc566c9e9ff8e5eb778810739
UNfassbarer <djonas2810@gmail.com>
2024-12-15 22:30:44 +0100

    MBT

--- Js/Mbt_force_calculation/calc.js
+++ Js/Mbt_force_calculation/calc.js
@@ -1,39 +1,62 @@
-let kraft_anzahl = prompt("Bitte Anzahl der wirkenden Kräfte eingeben");
-let kraft = prompt("Bitte jede kraft, mit `,` getrennt eingaben").split(",");
-let kraft_winkel = prompt("Bitte Winkel der einzelnen kräfte, durch `,` getrennt eingeben").split(",");
-let kraft_zähler = 0;
-let sin_a,cos_a,F_x,F_y;
+/*// Eingabefeld erstellen
+let inputField = document.createElement("input");
+inputField.setAttribute("type", "text");
+inputField.setAttribute("placeholder", "Geben Sie etwas ein");
 
+// Textarea erstellen
+let textArea = document.createElement("textarea");
+textArea.setAttribute("placeholder", "Geben Sie hier mehr Text ein");
+
+// Button erstellen
+let button = document.createElement("button");
+button.innerHTML = "Klicken Sie mich!";
+button.addEventListener("click", function() {
+    alert("Button wurde geklickt!");
+});
+
+// Alle Elemente dem Body hinzufügen
+document.body.appendChild(inputField);
+document.body.appendChild(textArea);
+document.body.appendChild(button);
+*/
+let kraft_anzahl = prompt("Bitte Anzahl der wirkenden Kräfte eingeben");
+let kraft = prompt("Bitte jede kraft, mit `,` getrennt eingaben").split(",").map(Number);
+let kraft_winkel = prompt("Bitte Winkel der einzelnen kräfte, durch `,` getrennt eingeben").split(",").map(Number);
+let kraft_zähler = 0;
+let F_x,F_y,x_ges = 0,y_ges = 0;
 while(kraft_zähler < kraft_anzahl){
 kraft_zähler++
-document.write(" <br> Kraft: " + kraft_zähler + "<br>")
-SIN_A()
-COS_A()
-KRAFT_Y()
-KRAFT_X()
+document.write(`<h3>Teil-Kraft: ${kraft_zähler} </h3>`)
+let sin_a = SIN_A();
+let cos_a = COS_A();
+document.write(` Sinus von ${kraft_winkel[kraft_zähler -1]}°: ${sin_a} <br>`)
+document.write(` Cosinus von ${kraft_winkel[kraft_zähler -1]}°: ${cos_a} <br>`)
+F_y = KRAFT_Y(sin_a);
+F_x = KRAFT_X(cos_a);
+document.write(` Kraft in Y_Richtung: ${F_y} <br>`)
+document.write(` Kraft in X_Richtung: ${F_x} <br>`)
+x_ges += F_x
+y_ges += F_y
+document.write(`<br>`)
 }
-// document.write(Math.cos(kraft_winkel[0]) + "<br>");
-// document.write(kraft_winkel + " = "+ typeof(kraft_winkel) +  "<br>");
-// document.write(kraft_winkel.split(",") + " = " + typeof(kraft_winkel) +  "<br>");
-// document.write(kraft_winkel[0]+ " " + kraft_winkel[1]+ " " + kraft_winkel[2]+ " " + kraft_winkel[3]+ " <br> ");
-// document.write([...kraft_winkel] + " " + typeof([...kraft_winkel]));
-
+document.write(`<h3>Gesammtkraft und Winkel:</h3>`)
+document.write(`Gesammtkraft:<b> ${F_GES()} </b><br>`);
+document.write(`Winkel der Gesammtkraft:<b> ${F_ALPHA()}° </b>`);
 function SIN_A() {
-     sin_a = document.write("sin: " + Math.round(Math.sin(kraft_winkel[kraft_zähler -1] * (Math.PI / 180))*100)/100 + " Nr." + kraft_zähler + "<br>")
-} 
+     return Math.round(Math.sin(kraft_winkel[kraft_zähler - 1] * (Math.PI / 180)) * 1000) / 1000
+ }
 function COS_A() {
-     cos_a = document.write("cos: " + Math.round(Math.cos(kraft_winkel[kraft_zähler -1] * (Math.PI / 180))*100)/100 + " Nr." + kraft_zähler + "<br>")
+     return Math.round(Math.cos(kraft_winkel[kraft_zähler -1] * (Math.PI / 180))*1000)/1000
 }
-function KRAFT_Y() {
-    F_y = document.write("F_y: " + SIN_A()*kraft[kraft_zähler -1] + "<br>")
+function KRAFT_Y(sin_a) {
+    return Math.round(sin_a*kraft[kraft_zähler -1]*1000)/1000
 }
-function KRAFT_X() {
-F_x = document.write("F_x: " + COS_A()*kraft[kraft_zähler -1] + "<br>")
+function KRAFT_X(cos_a) {
+     return Math.round(cos_a*kraft[kraft_zähler -1]*1000)/1000
 }
-
-/*
-document.write(Math.sin(30 * (Math.PI / 180)) + "<br>");
-document.write(Math.sin(60 * (Math.PI / 180)) + "<br>");
-document.write(Math.sin(90 * (Math.PI / 180)) + "<br>");
-document.write(Math.sin(120 *(Math.PI / 180)) + "<br>");
-*/
+function F_GES() {
+     return Math.round(Math.sqrt(Math.pow(x_ges, 2)+ Math.pow(y_ges, 2))*1000)/1000
+}
+function F_ALPHA() {
+     return Math.round(Math.atan2(y_ges,x_ges) * (180 / Math.PI)*1000)/1000
+}
