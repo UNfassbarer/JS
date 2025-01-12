6246bf18f3effcee47e557b7d3516e7847b4606c
UNfassbarer <djonas2810@gmail.com>
2025-01-12 12:05:00 +0100

    new

--- Js/triangle/triangle.js
+++ Js/triangle/triangle.js
@@ -1,21 +1,21 @@
-function getInputLength(input){
-    const parts = Array.from(input.matchAll(/[\p{Emoji}\u200D\uFE0F]|./gu));
-    return parts.length;
-}
-function selfmade(){
+function triangle(){
+    function getInputLength(input){
+        const parts = Array.from(input.matchAll(/[\p{Emoji}\u200D\uFE0F]|./gu));
+        return parts.length;
+    }
 let symbol = prompt("Please insert symbol for tree:")
 let lines = prompt("Please insert size of the tree:\r[lines]")
 let symbol_add = symbol+symbol,counter = 0
 let space_anzahl= lines
 let space = getInputLength(symbol)
-function writeSpace(space){
+    function writeSpace(space){
     let final_space = ""
     while (space>0){
         space--
         final_space += "&nbsp;&nbsp;"
     }
     return final_space
-}
+    }
 while (counter<lines){
     space_anzahl--
     let space_counter = space_anzahl;
@@ -28,4 +28,4 @@ while (counter<lines){
     counter++
     }
 }
-confirm("Start Programm?")? selfmade():document.write("Okay :)")
+confirm("Start Programm?")? triangle():document.write("Okay :)")
