cd51ea6441b214619960f672ed745010adc2c3ed
Unfassbarer <141145010+UNfassbarer@users.noreply.github.com>
2025-01-13 10:27:07 +0100

    new

--- Js/do_while_for/school/numbers form 1 to n.js
+++ Js/do_while_for/school/numbers form 1 to n.js
@@ -1,10 +1,10 @@
 const one_to_n = () => {
-var input = prompt("Please insert value of n"), sum = 1;
+let input = prompt("Please insert value of n"), sum = 1;
 while (sum <= input ){
 document.write(sum + " ");
 sum++;
 }
-var sum_new = sum - input;
+let sum_new = sum - input;
 while (sum_new <= input ){
 document.write( "<br>" + sum_new + "<br>")
 sum_new++;
