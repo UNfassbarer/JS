be4689e75cb6fa52b093314bcf5e6906fa293004
UNfassbarer <djonas2810@gmail.com>
2025-04-22 21:53:41 +0200

    new

--- Js/html_and_js_and_css/MBT/script.js
+++ Js/html_and_js_and_css/MBT/script.js
@@ -1,84 +1,16 @@
-// function changeContent(category, div, file) {
-//     change_content_2(category)
-//     $(`#${category}`).hover(() => loadContent(category, div, file)
-//     );
-// }
-// function loadContent(category, div, file) {
-//     fetch(file)
-//         .then(response => response.text())
-//         .then(data => {
-//             const parser = new DOMParser();
-//             const doc = parser.parseFromString(data, 'text/html');
-//             const inhalt = doc.getElementById(category);
-//             document.getElementById(div).innerHTML = inhalt.innerHTML;
-//         })
-// }
-// function change_content_2(category) {
-//     document.getElementById(category).addEventListener("mouseover", function () {
-//         document.getElementById("headline").innerHTML = (`${category}`).toUpperCase();
-//     });
-// }
-
-
-// $('.submenu_special').each(function () {
-//     changeContent(`${this.id}`, "main_content", "test_1.html")
-// });
-
 const el1 = document.getElementById('show_calculator');
 const el2 = document.getElementById('calculator');
 
 el1.addEventListener('mouseenter', () => {
     el2.style.display = 'block';
 });
-function ClearAll(){
+function ClearAll() {
     el2.style.display = 'none';
+    Pin_note()
     document.querySelectorAll('.content-container').forEach(div => {
         div.classList.add('hidden_content');
     });
 }
-
-const infoBox = document.querySelector('.info-box');
-const notes = document.querySelector('.note-box');
-const noteContent = document.querySelector('.note-content')
-const h1 = document.querySelector('.h1')
-const note_button = document.querySelector('.B_notes')
-
-infoBox.addEventListener('mouseenter', () => {
-  notes.style.top = '230px';
-});
-infoBox.addEventListener('mouseleave', () => {
-  notes.style.top = '80px';
-});
-notes.addEventListener("mouseenter", ()=>{
-    notes.style.width = "150px";
-    notes.style.height = "150px";
-    noteContent.style.opacity = "1";
-    h1.style.opacity = "0";
-    h1.style.transition = "opacity 0.1s ease-in";
-    note_button.style.opacity = "1";
-
-})
-const box = document.getElementById('box');
-  const corner = document.getElementById('corner');
-
-  corner.addEventListener('mousedown', function (event) {
-    const startY = event.clientY;
-    const startHeight = box.offsetHeight;
-
-    function resize(event) {
-      box.style.height = startHeight + (event.clientY - startY) + 'px';
-    }
-
-    function stopResize() {
-      window.removeEventListener('mousemove', resize);
-      window.removeEventListener('mouseup', stopResize);
-    }
-
-    window.addEventListener('mousemove', resize);
-    window.addEventListener('mouseup', stopResize);
-  });
-
-
 
 function showContentDiv(divId) {
     document.querySelectorAll('.content-container').forEach(div => {
@@ -92,6 +24,82 @@ document.querySelectorAll('.submenu_special_left').forEach(element => {
         showContentDiv(`${event.target.id}` + "_content")
     });
 });
+
+const infoBox = document.querySelector('.info-box');
+const notes = document.querySelector('.note-box');
+const noteContent = document.querySelector('.note-content')
+const h1 = document.querySelector('.h1')
+const note_button = document.querySelector('.B_notes')
+
+infoBox.addEventListener('mouseenter', () => {
+    notes.style.top = '230px';
+});
+infoBox.addEventListener('mouseleave', () => {
+    notes.style.top = '80px';
+});
+
+noteContent.addEventListener('input', () => { AutoResize() })
+
+function AutoResize() {
+    if (noteContent.scrollHeight >= 60) {
+    notes.style.height = noteContent.scrollHeight + 100 + "px";
+    }
+}
+
+let zoom_after_note = false;
+
+notes.addEventListener("mouseenter", () => {
+    if (noteContent.scrollHeight < 60) {
+        notes.style.width = "150px";
+        notes.style.height = "150px";
+    }
+    if (zoom_after_note === true) {
+        notes.style.width = "150px";
+        notes.style.height = noteContent.scrollHeight + 100 + "px";
+        zoom_after_note = 0;
+    }
+    noteContent.style.opacity = "1";
+    h1.style.opacity = "0";
+    h1.style.transition = "opacity 0.1s ease-in";
+    note_button.style.opacity = "1";
+});
+let pinCounter = 0;
+function Pin_note() {
+    pinCounter++
+    PinLogic(pinCounter)
+    zoom_after_note = true;
+    // notes.style.width = "150px";
+    // notes.style.height = "150px";
+    document.getElementById("loding_circle").classList.add("clicked");
+    document.getElementById("noteButton").classList.add("clicked");
+    setTimeout(() => {
+        notes.style.width = "30px";
+        notes.style.height = "30px";
+        noteContent.style.opacity = "0";
+        h1.style.opacity = "1";
+        note_button.style.opacity = "0";
+        document.getElementById("noteButton").classList.remove("clicked");
+        document.getElementById("loding_circle").classList.remove("clicked");
+    }, 750);
+}
+function PinLogic(Pins) {
+    const newDiv = document.createElement('div');
+    newDiv.id = 'Pin_' + Pins;
+    newDiv.className = 'submenu_special_right';
+    newDiv.innerHTML = `<a href="#">Pin ${Pins}</a> <button class="Pin_Buttons" id="Button_Pin_${Pins}">C</button>`;
+    document.getElementById('add_pins').appendChild(newDiv);
+  }
+  function Reset_Pin_Counter(){
+
+
+
+  }
+  
+  document.getElementById('add_pins').addEventListener('click', event => {
+    if (event.target.classList.contains('Pin_Buttons')) {
+        event.target.parentElement.remove();
+    }
+  });
 
 let tention, force, area;
 const calculate = (input) => {
@@ -135,7 +143,6 @@ function showCalculator() {
     });
     document.getElementById("calculator").classList.remove('hidden_calc');
 }
-
 
 ["tention", "force", "area"].forEach(id => {
     document.getElementById(id).addEventListener("input", () => calculate(id));
